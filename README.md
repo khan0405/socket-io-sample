663eed5a0ffa4ba37876a19ae4c34e36616c3062
khan0405 <khan4257@gmail.com>
2015-09-25 05:46:22 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Socket.io Sample for Chatting app
+
+from [socket.io chat example](https://github.com/socketio/socket.io/tree/master/examples/chat)
---
+++ model/socket.js
@@ -0,0 +1,76 @@
+module.exports = IO;
+
+function IO(server) {
+    if (!(this instanceof IO)) return new IO(server);
+    this.init(server);
+}
+
+IO.prototype.init = function (server) {
+    var io = require('socket.io')(server);
+    io.on('connection', Connection);
+}
+
+// Chatroom
+// usernames which are currently connected to the chat
+var usernames = {};
+var numUsers = 0;
+function Connection(socket) {
+
+    var addedUser = false;
+
+    // when the client emits 'new message', this listens and executes
+    socket.on('new message', function (data) {
+        // we tell the client to execute 'new message'
+        socket.broadcast.emit('new message', {
+            username: socket.username,
+            message: data
+        });
+    });
+
+    // when the client emits 'add user', this listens and executes
+    socket.on('add user', function (username) {
+        // we store the username in the socket session for this client
+        socket.username = username;
+        // add the client's username to the global list
+        usernames[username] = username;
+        ++numUsers;
+        addedUser = true;
+        socket.emit('login', {
+            numUsers: numUsers
+        });
+        // echo globally (all clients) that a person has connected
+        socket.broadcast.emit('user joined', {
+            username: socket.username,
+            numUsers: numUsers
+        });
+    });
+
+    // when the client emits 'typing', we broadcast it to others
+    socket.on('typing', function () {
+        socket.broadcast.emit('typing', {
+            username: socket.username
+        });
+    });
+
+    // when the client emits 'stop typing', we broadcast it to others
+    socket.on('stop typing', function () {
+        socket.broadcast.emit('stop typing', {
+            username: socket.username
+        });
+    });
+
+    // when the user disconnects.. perform this
+    socket.on('disconnect', function () {
+        // remove the username from global usernames list
+        if (addedUser) {
+            delete usernames[socket.username];
+            --numUsers;
+
+            // echo globally that this client has left
+            socket.broadcast.emit('user left', {
+                username: socket.username,
+                numUsers: numUsers
+            });
+        }
+    });
+}
